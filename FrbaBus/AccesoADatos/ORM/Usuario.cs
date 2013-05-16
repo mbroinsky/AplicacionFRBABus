f4aa13075643d37d380a9ce7c85209a4b4acd5ae
mbroinsky <maxi87@gmail.com>
2013-05-16 15:39:38 -0300

    Update Usuario.cs

--- FrbaBus/AccesoADatos/ORM/Usuario.cs
+++ FrbaBus/AccesoADatos/ORM/Usuario.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Collections;
 using System.Data;
 
-namespace AccesoADatos.ORM
+namespace AccesoADatos.Orm
 {
     public class Usuario
     {
@@ -14,6 +14,8 @@ namespace AccesoADatos.ORM
         public String Password { get; set; }
         public String Rol { get; set; }
         public ArrayList Permisos { get; set; }
+        private Int16 IdRol;
+
 
         public Usuario()
         {
@@ -30,14 +32,26 @@ namespace AccesoADatos.ORM
             return Permisos.Contains(formulario);
         }
 
-        public bool getUsuario(String usuario)
+        public bool getUsuario(String nick)
         {
-            DataTable dt = Conector.Datos.EjecutarComandoADataTable("select * from NOT_NULL.usuario where USR_nick = '" + usuario + "';");
+            DataTable dt = Conector.Datos.EjecutarComandoADataTable("select * from NOT_NULL.usuario, " +
+                       " NOT_NULL.rol where USR_nick = '" + nick + "' AND USR_idRol = ROL_id;");
 
             if (dt.Rows.Count > 0)
             {
                 Id = Convert.ToInt16(dt.Rows[0]["USR_idUsuario"]);
                 NombreUsr = dt.Rows[0]["USR_Nombre"] + " " + dt.Rows[0]["USR_Apellido"];
+                Password = dt.Rows[0]["USR_Clave"];
+                Rol = dt.Rows[0]["ROL_nombre"];
+                IdRol = Convert.ToInt16(dt.Rows[0]["ROL_id"]);
+                
+                dt = Conector.Datos.EjecutarComandoADataTable("select * from NOT_NULL.funcXRol, " + 
+                       " NOT_NULL.funcionalidad where FXR_idRol = '" + IdRol.ToString() + 
+                       "' AND FXR_idFunc = FUN_id;");
+                       
+                for(int i = 0; i < dt.Rows.Count; i++)
+                    Permisos.Add(dt.Rows[i]["FUN_nombre"]);
+                            
                 return true;
             }
             else
